7c003260156923f9f5401c708a8a82a7baba267f
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-22 04:35:31 +0500

    2nd

--- retire.c
+++ retire.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char **argv){
+int main(int argv, char **argc){
 
 
 // Made by : Murtaza Tanveer
@@ -12,7 +12,13 @@ int main(int argc, char **argv){
 printf("\nMade by : MURTAZA TANVEER\nReg no  : SP23-BSE-096-2C");
 printf("\n\n____________________WELCOME TO MOBILE DATA USAGE TRACKER___________________\n\n\n");
 
+float bal = atof(argc[1]);
 
+float month_add = atof(argc[2]);
+
+float year_per = atof(argc[3]);
+
+float year_inf = atof(argc[4]);
 
 
 
