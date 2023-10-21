144fc495a3dec5d83a270194636223737d2c6962
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-22 04:26:35 +0500

    1st commit

---
+++ retire.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+
+
+// Made by : Murtaza Tanveer
+//Reg No   : SP23-BSE-096-2C
+// Date         10/22/2023
+
+
+printf("\nMade by : MURTAZA TANVEER\nReg no  : SP23-BSE-096-2C");
+printf("\n\n____________________WELCOME TO MOBILE DATA USAGE TRACKER___________________\n\n\n");
+
+
+
+
+
+}
